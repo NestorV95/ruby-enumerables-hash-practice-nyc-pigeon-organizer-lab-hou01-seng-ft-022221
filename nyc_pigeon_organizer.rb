09c0fd4f4c43a2b917f7ce9584923c165c75ea87
Nestor Venegas <nestor.venegas95@gmail.com>
2021-02-10 02:25:19 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,8 +1,8 @@
 require 'pry'
 
 def nyc_pigeon_organizer(data)
-  data.each_with_object({}) do |(key, value), a|
- a[key] = value+ 3
+  data.each_with_object({}) do |(key, value), final_array|
+ final_array[key] = value+ 3
 end
 binding.pry
 end
