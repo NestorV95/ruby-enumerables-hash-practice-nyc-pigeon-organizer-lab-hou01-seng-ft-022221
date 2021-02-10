2095e2e577bc41f9cb695349dc18e8a7a5f685f0
Nestor Venegas <nestor.venegas95@gmail.com>
2021-02-10 02:54:03 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -7,10 +7,10 @@ final_results = data.each_with_object({}) do |(key, value), final_array|
   value.each do |inner_key, names|
    names.each do |name|
     # binding.pry
-     if !final_array[name]
+     if! final_array[name]
        final_array = {}
      end
-     if !final_array[name][key]
+     if! final_array[name][key]
        final_array[name][key] = []
      end
      final_array[name][key].push(inner_key.to_s)
